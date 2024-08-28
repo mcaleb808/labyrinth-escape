936bef1283e0b3516212a1c1eecaddc7b7694e0c
mcaleb808 <mcaleb808@gmail.com>
2024-08-28 12:38:44 +0200

    Split Labyrinth escape logic

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,10 @@ export const initialLabyrinth = [
   ["0", "0", "1", "1", "1"],
   ["0", "0", "0", "0", "0"],
 ];
+
+export const directions = [
+  [0, 1], // right
+  [1, 0], // down
+  [0, -1], // left
+  [-1, 0], // up
+];
