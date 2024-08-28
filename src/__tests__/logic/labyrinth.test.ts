b98e868c9166a9264c581994675887fb9f597776
mcaleb808 <mcaleb808@gmail.com>
2024-08-28 17:01:30 +0200

    Cleanup and Increase Test Coverage

--- src/__tests__/logic/labyrinth.test.ts
+++ src/__tests__/logic/labyrinth.test.ts
@@ -1,16 +1,10 @@
+import { initialLabyrinth } from "@/lib/constants";
+import { findStartPosition } from "@/lib/helpers";
 import findShortestPath from "@/lib/labyrinthEscape";
 
 describe("Labyrinth Escape", () => {
   it("should return the shortest path length from S to E", () => {
-    const labyrinth = [
-      ["S", "0", "1", "0", "E"],
-      ["1", "0", "1", "0", "1"],
-      ["1", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "1"],
-      ["0", "0", "0", "0", "0"],
-    ];
-
-    const result = findShortestPath(labyrinth);
+    const result = findShortestPath(initialLabyrinth);
 
     expect(result?.length).toBe(8);
   });
@@ -28,4 +22,17 @@ describe("Labyrinth Escape", () => {
 
     expect(result?.length).toBe(-1);
   });
+
+  it("should throw an error when the start position 'S' is not found", () => {
+    const labyrinthWithoutS = [
+      ["0", "0", "1"],
+      ["1", "0", "0"],
+      ["0", "0", "E"]
+    ];
+
+    expect(() => findStartPosition(labyrinthWithoutS)).toThrow(
+      "Start position 'S' not found in the labyrinth."
+    );
+  });
+
 });
